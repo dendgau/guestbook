dc5ccf51e1b10309b3a4f62dcb9e339baf883363
dendgau <dendgau@gmail.com>
2015-03-18 13:29:04 +0700

    Test unit 2

--- guestbook_app/tests/test_models.py
+++ guestbook_app/tests/test_models.py
@@ -10,13 +10,47 @@ class TestBassClass(unittest.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
-        self.testbed.init_datastore_v3_stub(consistency_policy=self.policy)
 
     def tearDown(self):
         self.testbed.deactivate()
 
 
+class TestClassGuestbook(TestBassClass):
+
+    def test_get_guestbook_key(self):
+        key_before = ndb.Key('Guestbook', "test_get_key")
+        key_after = Guestbook.get_guestbook_key("test_get_key")
+        assert key_after == key_before
+
+    def test_get_guestbook_by_name(self):
+        guestbook_name_before = "test_get_by_name"
+        Guestbook.add_new_book(guestbook_name_before)
+        guestbook_name_after = Guestbook.get_guestbook_by_name(guestbook_name_before)
+        assert guestbook_name_after == guestbook_name_before
+
+    def test_check_is_exist(self):
+        guestbook_name_before = "test_check_is_exist"
+        Guestbook.add_new_book(guestbook_name_before)
+        is_exist = Guestbook.check_is_exist(guestbook_name_before)
+        assert is_exist is True
+
+
 class TestClassGreeting(TestBassClass):
+
+    def test_get_greeting_with_cursor(self):
+        for num in range(0, 25, 1):
+            dictionary = {
+                "guestbook_name": AppConstants.get_default_guestbook_name(),
+                "content": str(num)
+            }
+            Greeting.put_from_dict(dictionary)
+
+        greetings, next_cursor, is_more = Greeting.get_greeting_with_cursor(None, 20)
+        assert (
+            greetings is not None and len(greetings) == 20 and
+            next_cursor is not None and is_more is True
+        )
+
 
     def test_get_greetings(self):
         greetings = Greeting.get_greetings(AppConstants.get_default_guestbook_name())
@@ -32,13 +66,13 @@ class TestClassGreeting(TestBassClass):
 
     def test_delete_greeting(self):
         dictionary = {
-            "dictionary": AppConstants.get_default_guestbook_name(),
+            "guestbook_name": AppConstants.get_default_guestbook_name(),
             "content": "test"
         }
         greeting = Greeting.put_from_dict(dictionary)
         dictionary_test = {
             "greeting_id": str(greeting.key.id),
-            "guestbook_name": str(greeting.key.parent.id)
+            "guestbook_name": AppConstants.get_default_guestbook_name()
         }
         is_delete = Greeting.delete_greeting(dictionary_test)
         assert(is_delete is True)
@@ -50,18 +84,39 @@ class TestClassGreeting(TestBassClass):
         }
         greeting = Greeting.put_from_dict(dictionary)
 
-        dict_update = {
-            "greeting_id": greeting.key.id,
-            "guestbook_name": greeting.key.parent.id,
-            "author": None,
+        dict_before = {
+            "greeting_id": str(greeting.key.id),
+            "guestbook_name": AppConstants.get_default_guestbook_name(),
+            "author": str(greeting.author),
             "content": "test_version2"
         }
-        greeting_after_update = Greeting.update_greeting(dict_update)
+        greeting_after = Greeting.update_greeting(dict_before)
         assert(
-            dict_update["greeting_id"] == greeting_after_update.key.id and
-            dict_update["guestbook_name"] == greeting_after_update.key.parent.id and
-            dict_update["author"] == greeting_after_update.author and
-            dict_update["content"] == greeting_after_update.content
+            dict_before["greeting_id"] == str(greeting_after.key.id) and
+            dict_before["author"] == str(greeting_after.author) and
+            dict_before["content"] == str(greeting_after.content)
+        )
+
+    def test_get_greeting(self):
+        dictionary = {
+            "guestbook_name": AppConstants.get_default_guestbook_name(),
+            "content": "test"
+        }
+        greeting = Greeting.put_from_dict(dictionary)
+
+        dict_before = {
+            "greeting_id": str(greeting.key.id),
+            "guestbook_name": AppConstants.get_default_guestbook_name(),
+            "author": str(greeting.author),
+            "content": str(greeting.content)
+        }
+        greeting_after = Greeting.get_greeting(
+            greeting.key.id, AppConstants.get_default_guestbook_name()
+        )
+        assert(
+            dict_before["greeting_id"] == str(greeting_after.key.id) and
+            dict_before["author"] == str(greeting_after.author) and
+            dict_before["content"] == str(greeting_after.content)
         )
 
 
